4240661332faf7d75af0c35159444d6c35146482
Chaitanyaphad <chaitanyaphad2003@gmail.com>
2024-06-30 23:38:47 +0530

    updated navbar and hero section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react'
 import './Navbar.css';
-import logo from '../../assets/logo.svg'
 import menu_open from '../../assets/menu_open.svg'
 import menu_close from '../../assets/menu_close.svg'
 
@@ -18,7 +17,7 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <img src={logo} alt="" />
+            <p id='logo'>Chaitanya</p>
             <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open'/>
             <ul ref={menuRef} className='nav-menu'>
                 <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
